f4f0e2e61adf2d3cbccf7fa5cf8a07db403895e1
thegove <gove.allen@gmail.com>
2021-05-11 15:23:47 -0600

    sorted income statement

--- url.js
+++ url.js
@@ -1,4 +1,4 @@
 const gas_url=
-"https://script.google.com/macros/s/AKfycbyKM3YPjUMbAmWJudPCM1gOdajZM_jWeh9K6M9jyXYJ8DC8ZkYUOF_KoTHvqfEBn2U/exec"
+"https://script.google.com/macros/s/AKfycbzn4_5r8o_xti8GGHxn4jOgIaJNDQAP4Dg04V438HMu_S9fmT4kBz3fVjFdlUOyRBHE/exec"
 // This refers to the google apps script named
 // google authenticated airtable owned by gove@colonialheritage.org
