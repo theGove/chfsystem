254da2e37538e24b38703691bc5121b7fb657f5f
Gove Allen <gove.allen@gmail.com>
2022-01-09 11:38:59 -0700

    dev

--- js/url.js
+++ js/url.js
@@ -1,7 +1,7 @@
 
 
 
-const deployment_id="AKfycbxOcWwlB05rYoeDQnakH3DHLfZ8UO0GDFLmLG1bvTQbF7H1jvO3n7YS7g62dRYXf3es"
+const deployment_id="AKfycbzr71IS94vQc6M8ltjH1G4vv7j6e4oFTNkI891WphsPvXI7qzdqEYd9YvIf_SR7Ezg"
 const gas_url="https://script.google.com/macros/s/" + deployment_id +"/exec"
 // This refers to the google apps script named
 // google authenticated airtable owned by gove@colonialheritage.org
