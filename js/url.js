68880e4e95f4661bb36125a40edd7ef7132ab61e
thegove <gove.allen@gmail.com>
2025-06-13 14:15:18 -0600

    pat

--- js/url.js
+++ js/url.js
@@ -2,7 +2,7 @@
 
 // 2022: AKfycbzr71IS94vQc6M8ltjH1G4vv7j6e4oFTNkI891WphsPvXI7qzdqEYd9YvIf_SR7Ezg
 
-const deployment_id="AKfycbwdg04IAGmJrAMjXlerXYZhfywDPWsTOAH_WSf-ZIeg-qGwikZTyS3tG8uPQog28MW6"
+const deployment_id="AKfycbzv6GZaki28MCIZYbyJlICAPer4yQB2N3iVxMbp-_auQNnrv5sL8jOsR4QA2vqCwhbt"
 const gas_url="https://script.google.com/macros/s/" + deployment_id +"/exec"
 // This refers to the google apps script named
 // google authenticated airtable 2 script  owned by gove@colonialheritage.org
