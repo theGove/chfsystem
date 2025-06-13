1a732a9b7312a177d53d9e4371ee47aeec75005d
thegove <gove.allen@gmail.com>
2025-06-13 14:34:10 -0600

    new spy dat

--- js/url.js
+++ js/url.js
@@ -2,7 +2,7 @@
 
 // 2022: AKfycbzr71IS94vQc6M8ltjH1G4vv7j6e4oFTNkI891WphsPvXI7qzdqEYd9YvIf_SR7Ezg
 
-const deployment_id="AKfycbzv6GZaki28MCIZYbyJlICAPer4yQB2N3iVxMbp-_auQNnrv5sL8jOsR4QA2vqCwhbt"
+const deployment_id="AKfycbwUHqBAmnGfE8BohnvA3tGH474MoHHbtBWtgXXFrOjYiei316bYDj5VfNCMVvGE-meh"
 const gas_url="https://script.google.com/macros/s/" + deployment_id +"/exec"
 // This refers to the google apps script named
 // google authenticated airtable 2 script  owned by gove@colonialheritage.org
